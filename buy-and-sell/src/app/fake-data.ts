d47b452c269cbcd55f9b9c2a189fb74a0c5a7d1e
burakerdgn1 <burakerdogan749@gmail.com>
2023-08-29 15:39:03 +0300

    Project created.
    -Components are initialized.

---
+++ buy-and-sell/src/app/fake-data.ts
@@ -0,0 +1,22 @@
+import { Listing } from "./models/list-item"
+
+export const fakeListings : Array<Listing>=[
+    {
+        id:'123',
+        name:'Old Boat',
+        description:'A very old boat. Bargain price',
+        price:700
+    },
+    {
+        id:'345',
+        name:'Computer',
+        description:'A classic computer from 1990s',
+        price:50
+    },
+    {
+        id:'456',
+        name:'Basketball Hoop',
+        description:'Good condition, free delivery',
+        price:5
+    }
+]
