b8116933be07c39d151111108a1cc62b7c5e027f
burakerdgn1 <burakerdogan749@gmail.com>
2023-08-31 09:03:24 +0300

    My Listings Page is created, default content is initialized.
    New Listing Page is created, default content is initialized.

--- buy-and-sell/src/app/my-listings-page/my-listings-page.component.ts
+++ buy-and-sell/src/app/my-listings-page/my-listings-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../models/list-item';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-my-listings-page',
   templateUrl: './my-listings-page.component.html',
   styleUrls: ['./my-listings-page.component.css']
 })
-export class MyListingsPageComponent {
+export class MyListingsPageComponent implements OnInit {
+  ngOnInit(): void {
+    this.listings=fakeListings;
+  }
+  listings:Listing[]=[];
+  onDeleteClicked(listingId:string){
+    alert(`Deleting the listing with id: ${listingId}`);
+
+  }
 
 }
--- buy-and-sell/src/app/new-listing-page/new-listing-page.component.ts
+++ buy-and-sell/src/app/new-listing-page/new-listing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-listing-page',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-listing-page.component.css']
 })
 export class NewListingPageComponent {
+  name:string='';
+  description:string='';
+  price:string='';
+  constructor(private router:Router){}
 
+  onSubmit(){
+    alert('Creating new listing...');
+    this.router.navigateByUrl('/my-listings')
+
+  }
 }
