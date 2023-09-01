fe6b9c7ff0fab9379d95af3fd9fb885061286a25
burakerdgn1 <burakerdogan749@gmail.com>
2023-09-01 11:19:39 +0300

    Nav-bar created. Listing data form created
    -Default navbar content is initialized.
    -Default list data form content is initialized.
    -Edit listing page updated.
    -New listing page updated.

--- buy-and-sell/src/app/edit-listing-page/edit-listing-page.component.ts
+++ buy-and-sell/src/app/edit-listing-page/edit-listing-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Listing } from '../models/list-item';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-edit-listing-page',
   templateUrl: './edit-listing-page.component.html',
   styleUrls: ['./edit-listing-page.component.css']
 })
-export class EditListingPageComponent {
+export class EditListingPageComponent implements OnInit{
+  listing:Listing|any;
+  constructor(private router:Router,private route:ActivatedRoute){
+
+  }
+  ngOnInit(): void {
+    const id=this.route.snapshot.paramMap.get('id');
+    this.listing=fakeListings.find(listing=>listing.id===id);
+  }
+  onSubmit(){
+    alert('Saving changes to the listing');
+    this.router.navigateByUrl('/my-listings');
+  }
+  
 
 }
---
+++ buy-and-sell/src/app/listing-data-form/listing-data-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Listing } from '../models/list-item';
+
+@Component({
+  selector: 'app-listing-data-form',
+  templateUrl: './listing-data-form.component.html',
+  styleUrls: ['./listing-data-form.component.css']
+})
+export class ListingDataFormComponent implements OnInit {
+  name:string='';
+  description:string='';
+  price:string='';
+  @Input() buttonText='';
+  @Input() currentName='';
+  @Input() currentDescription='';
+  @Input() currentPrice='';
+  @Output() onSubmit=new EventEmitter<Listing>();
+  constructor(private router:Router){}
+  ngOnInit(): void {
+    this.name=this.currentName;
+    this.description=this.currentDescription;
+    this.price=this.currentPrice;
+  }
+
+  onCreateListing(){
+    this.onSubmit.emit({id:'',name:this.name,description:this.description,price:Number(this.price)})
+
+  }
+
+}
